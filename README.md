1dc37685689339b13264e9a9871076a0e2273f34
Michael Van Der Velden <mikevdv@vandie.co.uk>
2017-10-01 12:12:26 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Thanks Translate
+Vandie Studios Ltd makes use of this library to translate Thanks™ to a range of languages. We decided it would makes more sense to open source it and allow our users to help ensure that the system made sense in their language also. For that reason, we offer Thanks Translate.
+
+##Contribution
+Before makinging any pull requests please ensure you have added your language code to the `lang.js` file and your language file fits the form shown in `en.json`.
---
+++ lang.js
@@ -0,0 +1,23 @@
+var fs = require('fs')
+
+var langDetails = (req) => {
+    return new Promise( (resolve,reject) => {
+        let rtls = ['ar','arc','dv','far','ha','he','khw','ks','ku','ur','yi']
+        let accepted = ['en']
+        let lang = req.acceptsLanguages(accepted)
+        lang = lang ? lang : 'en'
+        let dir = rtls.includes(lang) ? 'rtl' : 'ltr'
+        getLangFile(lang).then( langData => resolve( { lang: lang, dir:dir, data:langData } ))
+        .catch(err => reject(err))
+    })
+}
+
+var getLangFile = lang => {
+    return new Promise( (resolve,reject) => {
+        fs.readFile(__dirname+'/langFiles/'+lang+'.json', function(err, data){
+            err ? reject(err) : resolve(JSON.parse(data))
+        })
+    })
+}
+
+module.exports = langDetails
